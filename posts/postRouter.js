fc4982ca5c7b3ce977a23ae31cb83e172fed5cd5
anthony <anthony24.kwhomes@gmail.com>
2019-06-05 15:03:27 -0700

    Updated index, json, post, and server files.

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,16 +1,75 @@
-const express = 'express';
+const express = require('express');
+const Users = require('../users/userDb');
+const Post = require('./postDb');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const posts = await Post.get();
+    try {
+        res.status(200).json(posts)
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: 'Cannot find posts' })
+    }
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    const posts = await Post.getById(req.params.id);
+    try {
+        res.status(200).json(posts)
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: 'Cannot find post'})
+    }
 
 });
 
-router.delete('/:id', (req, res) => {
+router.post('/', async (req, res) => {
+    const newPost = await Post.insert(req.body);
+    try {
+        res.status(201).json({newPost})
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: 'Cannot add post'})
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        if (!req.body.text) {
+            return res.status(400).json({ errorMessage: 'Please provide text for post.'})
+        }
+        const post = await post.update(req.params.id, req.body)
+        if(post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({ message: 'Post not found'})
+        }
+    }
+    catch(err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Updating error'
+        })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await post.remove(req.params.id)
+        if (post > 0) {
+            res.status(200).json({ message: 'Post has been deleted'})
+        } else {
+            res.status(404).json({ message: 'Cant find post with this ID.'})
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Error! Cannot delete post.'
+        })
+    }
 
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = 'express';
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const server = express();
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(morgan('dev'));
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
